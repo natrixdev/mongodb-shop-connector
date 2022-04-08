9db6396978683bb52095fe694c56cba008f15a83
natrix_dev <discordlenko@gmail.com>
2022-04-08 19:16:19 +0200

    Update script1.js

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,1 +1,11 @@
-js
+const express = require('express')
+
+module.exports = {
+  create: () => express().set('json spaces', 2),
+  defaults: require('./defaults'),
+  router: require('./router'),
+  rewriter: require('./rewriter'),
+  bodyParser: require('./body-parser'),
+}
+
+// change the files with your names 
